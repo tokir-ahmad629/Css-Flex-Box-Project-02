fcb21a438bfb690b2f5e027991849de508f45cdd
Shakib Bhuiyan <78194114+tokir-ahmad629@users.noreply.github.com>
2022-09-19 01:29:48 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    let click = document.querySelector("ul");
+    click.classList.toggle("active");
+    let hidebtn = document.querySelector(".togol_icons i");
+    hidebtn.classList.toggle("fa-times");
+
+}
+
